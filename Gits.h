c45c9d23b1ec64f475bf559e785faadf4c2a36ba
ktessma21 <ktessma21@gmail.com>
2025-12-19 02:22:51 -0600

    Adjusted IDEAS: Git loads all the objects from the database lazily(on demand), and mentions the reason because it wants to make it faster or some sense. We change it to load the information from the repo eagerly.

---
+++ Gits.h
@@ -0,0 +1,10 @@
+
+// An array of commits returned from the library
+
+// typedef struct git_commit git_commit;
+
+
+// typedef struct git_commitarray {
+//         git_commit *const *commits
+//         size_t count
+//     };
